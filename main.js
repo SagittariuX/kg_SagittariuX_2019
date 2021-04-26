2ec0e3f76ff4f47c8ef5fb31d442acf33a4ae830
Aaron An <aaronan1996@gmail.com>
2021-04-26 18:10:00 -0400

    created a readme

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-const args = process.argv.slice(2);
+//Not sure what environment this will be run on
+//Using the most basic feature of node
+const args = process.argv.slice(2).map((arg) => parseInt(arg, 10));
+
+//Dictionary for number to word tanslation
+const dict = {
+  0: "Zero",
+  1: "One",
+  2: "Two",
+  3: "Three",
+  4: "Four",
+  5: "Five",
+  6: "Six",
+  7: "Seven",
+  8: "Eight",
+  9: "Nine",
+};
+
+/**
+ * Converts an array of nums to a
+ * @param {number[]} nums
+ * @returns {string[]}
+ */
+const numbersToStrings = (nums) => {
+  const strings = nums.map((num) => {
+    const stringNum = num.toString();
+    return stringNum
+      .split("")
+      .map((digit) => dict[digit])
+      .join("");
+  });
+
+  return strings;
+};
+
+//Final stdout format
+console.log(numbersToStrings(args).join(","));
